50fdb8b1acccf798f7c25d2ab350a3250fede3e2
Sergio Benavent Mascarell <serbemas@alumno.upv.es>
2016-02-12 09:53:01 +0100

    Sem2 Finished

--- app/src/main/java/com/serbemas/amuii/myaiot/MainActivity.java
+++ app/src/main/java/com/serbemas/amuii/myaiot/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +50,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    synchronized void imprimir(final String cad) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                TextView ll = (TextView) findViewById(R.id.TextViewEstado);
+                ll.append(cad);
+                ScrollView ll1 = (ScrollView) findViewById(R.id.scrollView);
+                ll1.fullScroll(View.FOCUS_DOWN);
+            }});
     }
 }
